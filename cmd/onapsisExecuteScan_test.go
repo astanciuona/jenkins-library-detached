02d90046fa6fcab240e213e906a344e6d615f507
astanciu <andrei.stanciu@onapsis.com>
2025-02-05 14:22:28 +0200

    uncommet tests

--- cmd/onapsisExecuteScan_test.go
+++ cmd/onapsisExecuteScan_test.go
@@ -1,49 +1,54 @@
 package cmd
 
-// "github.com/SAP/jenkins-library/pkg/mock"
+import (
+	"testing"
+
+	"github.com/SAP/jenkins-library/pkg/mock"
+	"github.com/stretchr/testify/assert"
+)
 
 type onapsisExecuteScanMockUtils struct {
-	// *mock.ExecMockRunner
-	// *mock.FilesMock
+	*mock.ExecMockRunner
+	*mock.FilesMock
 }
 
-// func newOnapsisExecuteScanTestsUtils() onapsisExecuteScanMockUtils {
-// 	utils := onapsisExecuteScanMockUtils{
-// 		ExecMockRunner: &mock.ExecMockRunner{},
-// 		FilesMock:      &mock.FilesMock{},
-// 	}
-// 	return utils
-// }
+func newOnapsisExecuteScanTestsUtils() onapsisExecuteScanMockUtils {
+	utils := onapsisExecuteScanMockUtils{
+		ExecMockRunner: &mock.ExecMockRunner{},
+		FilesMock:      &mock.FilesMock{},
+	}
+	return utils
+}
 
-// func TestRunOnapsisExecuteScan(t *testing.T) {
-// 	t.Parallel()
+func TestRunOnapsisExecuteScan(t *testing.T) {
+	t.Parallel()
 
-// 	t.Run("happy path", func(t *testing.T) {
-// 		t.Parallel()
-// 		// init
-// 		config := onapsisExecuteScanOptions{}
+	t.Run("happy path", func(t *testing.T) {
+		t.Parallel()
+		// init
+		config := onapsisExecuteScanOptions{}
 
-// 		utils := newOnapsisExecuteScanTestsUtils()
-// 		utils.AddFile("file.txt", []byte("dummy content"))
+		utils := newOnapsisExecuteScanTestsUtils()
+		utils.AddFile("file.txt", []byte("dummy content"))
 
-// 		// test
-// 		err := runOnapsisExecuteScan(&config, nil, utils)
+		// test
+		err := runOnapsisExecuteScan(&config, nil, utils)
 
-// 		// assert
-// 		assert.NoError(t, err)
-// 	})
+		// assert
+		assert.NoError(t, err)
+	})
 
-// 	t.Run("error path", func(t *testing.T) {
-// 		t.Parallel()
-// 		// init
-// 		config := onapsisExecuteScanOptions{}
+	t.Run("error path", func(t *testing.T) {
+		t.Parallel()
+		// init
+		config := onapsisExecuteScanOptions{}
 
-// 		utils := newOnapsisExecuteScanTestsUtils()
+		utils := newOnapsisExecuteScanTestsUtils()
 
-// 		// test
-// 		err := runOnapsisExecuteScan(&config, nil, utils)
+		// test
+		err := runOnapsisExecuteScan(&config, nil, utils)
 
-//		// assert
-//		assert.EqualError(t, err, "cannot run without important file")
-//	})
-// }
+		// assert
+		assert.EqualError(t, err, "cannot run without important file")
+	})
+}
